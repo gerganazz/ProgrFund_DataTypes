833adb942c674c0e722f64942e059713763cf81a
Test-TEst <teste23dsadtfwri2jj3je@gmail.com>
2016-09-26 16:19:49 +0300

    Added projects

---
+++ 11_ConvertSpeedUnits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+   
+class Program
+{
+    static void Main()
+    {
+        float distMeters = float.Parse(Console.ReadLine());
+        int hours = int.Parse(Console.ReadLine());
+        int min = int.Parse(Console.ReadLine());
+        int sec = int.Parse(Console.ReadLine());
+
+        int timeSec = hours * 60 * 60 + min * 60 + sec;
+        float timeHours = timeSec / 3600f;
+        float distKm = distMeters / 1000;
+        float distMiles = distMeters / 1609;
+
+        float speedMPS = distMeters / timeSec;
+        float speedKPH = distKm / timeHours;
+        float speedMPH = distMiles / timeHours;
+
+        Console.WriteLine(speedMPS);
+        Console.WriteLine(speedKPH);
+        Console.WriteLine(speedMPH);
+    }                                     
+}
+
---
+++ 19_TheaThePhotographer/TheaThePhotographer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+  
+public class TheaThePhotographer
+{
+   public static void Main()
+    {
+        int allPictures = int.Parse(Console.ReadLine());
+        int filterTimePerPic = int.Parse(Console.ReadLine());
+        byte filterFactor = byte.Parse(Console.ReadLine());
+        int uploadTimePerPic = int.Parse(Console.ReadLine());
+               
+        long filterTime = allPictures * filterTimePerPic;
+        int goodPictures = (int)Math.Ceiling(allPictures * filterFactor / 100m);
+        long uploadTime = goodPictures * uploadTimePerPic;
+        long totalTime = filterTime + uploadTime;
+
+        byte sec = (byte) (totalTime % 60);
+        long tempTime = totalTime / 60;
+        byte min = (byte)(tempTime % 60);
+        tempTime /= 60;
+        byte hours = (byte)(tempTime % 60); 
+        tempTime /= 60;
+        int days = (int)(tempTime / 24);
+
+        Console.WriteLine("{0:0}:{1:00}:{2:00}:{3:00}", days, hours, min, sec);
+    }
+}
+
